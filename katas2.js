7a300aebfd0710000b3520a2023ded15ee32549f
rodrigocezzar <rodrigocezzar85@gmail.com>
2021-08-10 19:06:32 -0300

    resolução_rodrigo_abreu

--- katas2.js
+++ katas2.js
@@ -1,37 +1,48 @@
 // comece a criar a sua função add na linha abaixo
 
 
-// descomente a linha seguinte para testar sua função
-// console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
+function add(a, b) {  
+    return a + b; 
+}
+
+console.log(add(3, 5), 'add');
 
 
 // comece a criar a sua função multiply na linha abaixo
 
+function multiply(a, b) { 
+    let soma = 0;
+    for(let i = 0; i < b; i++) {   
+       soma = add(soma, a);              
+    } 
+        return soma;
+}
 
-// descomente a linha seguinte para testar sua função
-// console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
+console.log(multiply(4, 6), 'multiply');
 
 
 // comece a criar a sua função power na linha abaixo
 
+function power(x, n) { 
+    let multiplica = 1;
+    for(let i = 0; i < n; i++) {
+        multiplica = multiply(multiplica, x)
+    }
+    return multiplica;
+}
 
-// descomente a linha seguinte para testar sua função
-// console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
+console.log(power(3, 4), 'Power');
 
 
 // comece a criar a sua função factorial na linha abaixo
 
-
-// descomente a linha seguinte para testar sua função
-// console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
-
-
-/**
- * BONUS (aviso: o grau de dificuldade é bem maior !!!)
- */
-
-// crie a função fibonacci
+function factorial(z) {
+    let fator = z ;
+    for(let i = 1; i < z; i++) {
+        fator = multiply(fator, i)
+    }
+        return fator;
+}
+console.log(factorial(5), 'factorial');
 
 
-// descomente a linha seguinte para testar sua função
-// console.assert(fibonacci(7) === 13, 'A função fibonacci não está funcionando como esperado');
